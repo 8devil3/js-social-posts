6342d67b90edae59d0aa8d600c72700be463e117
8devil3 <8dev.il3@gmail.com>
2022-03-08 17:28:13 +0100

    corertte date

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "created": "2021-06-25"
+        "created": "2021-25-06"
     },
     {
         "id": 2,
@@ -19,7 +19,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=10"
         },
         "likes": 120,
-        "created": "2021-09-03"
+        "created": "2021-03-09"
     },
     {
         "id": 3,
@@ -30,7 +30,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=20"
         },
         "likes": 78,
-        "created": "2021-05-15"
+        "created": "2021-15-05"
     },
     {
         "id": 4,
@@ -41,7 +41,7 @@ const posts = [
             "image": null
         },
         "likes": 56,
-        "created": "2021-04-03"
+        "created": "2021-03-04"
     },
     {
         "id": 5,
@@ -52,7 +52,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
-        "created": "2021-03-05"
+        "created": "2021-05-05"
     }
 ];
 
@@ -72,7 +72,7 @@ function printPost(postContent, index){
 
     let oldDate = postContent.created;
 
-    let newDate = oldDate.slice(-2) + '/' + oldDate.slice(5,7) + '/' + oldDate.slice(0,4);
+    let newDate =  oldDate.slice(5,7) + '/' + oldDate.slice(-2) + '/' + oldDate.slice(0,4);
 
     let post = document.createElement('div');
     post.classList.add('post');
