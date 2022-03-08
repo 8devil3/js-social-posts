e2e6afabfb05918881fdb447bcb5b773738f8ca0
8devil3 <8dev.il3@gmail.com>
2022-03-08 15:05:29 +0100

    milestone 1

--- main.js
+++ main.js
@@ -57,3 +57,46 @@ const posts = [
 ];
 
 
+const postList = document.querySelector('#container');
+
+for (let i = 0; i < posts.length; i++) {
+    printPost(posts[i]);
+}
+
+function printPost(postContent){
+    let post = document.createElement('div');
+    post.classList.add('post');
+
+    post.innerHTML = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${postContent.author.image}" alt="${postContent.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${postContent.author.name}</div>
+                    <div class="post-meta__time">${postContent.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${postContent.content}</div>
+        <div class="post__image">
+            <img src="${postContent.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${postContent.likes}</b> persone
+                </div>
+            </div> 
+        </div> `;
+
+    postList.appendChild(post);
+
+}
